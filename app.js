ccdb67c25eb33b096a7be222be4ddc9cf1c8de7b
AnoySarkar <123885890+AnoySarkar@users.noreply.github.com>
2025-02-10 10:09:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,10 +27,12 @@ boxes.forEach((box) => {
    if(turnO){
       box.innerText = "O";
       turnO = false;
+      box.style.backgroundColor = "yellow";
     } 
     else{
       box.innerText = "X";
       turnO = true;
+      box.style.backgroundColor = "blue";
     }
     box.disabled = true;
 
