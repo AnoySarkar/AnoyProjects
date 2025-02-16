d9396d708dd7007b5a21bde8ba2749867961cfa8
AnoySarkar <123885890+AnoySarkar@users.noreply.github.com>
2025-02-17 00:19:30 +0600

    Update ExpenseList.js

--- ExpenseList.js
+++ ExpenseList.js
@@ -1,4 +1,10 @@
-let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+let expList = JSON.parse(localStorage.getItem('expList')) || [];
+
+let dateExp = JSON.parse(localStorage.getItem('dateExp')) || [{
+  dateE: '',
+  exp: 0,
+}];
+
 
   document.querySelector('.js-input')
     .addEventListener("keydown",function (event){
@@ -9,7 +15,12 @@ let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
 
     document.addEventListener("DOMContentLoaded", function() {
       renderTodo();
+      renderDateExp();
+
+
     });
+
+    
 
 function addTodo() {
 
@@ -21,18 +32,42 @@ function addTodo() {
 
   let inputDate = document.querySelector('.js-input-date');
   let date = inputDate.value;
+  let money = parseInt(name.replace(/\D/g, ""));
+
+  addDateExp(date,money);
+  localStorage.setItem('dateExp', JSON.stringify(dateExp));
 
 
-  todoList.push({
+  expList.push({
     name,
     date
   });
   
   inputTodo.value = '';
 
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+  localStorage.setItem('expList', JSON.stringify(expList));
 
   renderTodo();
+  renderDateExp();
+}
+
+function addDateExp(date, money){
+  
+  for (i=0; i < dateExp.length; i++){
+    let dateObject = dateExp[i];
+    let {dateE} = dateObject;
+
+    if (dateE === date){
+      dateExp[i].exp += money;
+      return;
+    }}
+
+    dateExp.push({
+      dateE: date,
+      exp: money
+    });
+
+
 }
 
 
@@ -42,36 +77,87 @@ function renderTodo() {
   let total = 0;
   let totalItem = [];
   
-    for (let i = 0; i < todoList.length; i++){
+    for (let i = 0; i < expList.length; i++){
       
-      let todoObject = todoList[i];
+      let todoObject = expList[i];
       let {name , date} = todoObject;
 
       let money = parseInt(name.replace(/\D/g, ""));
       let todoName = name.replace(/\d/g, '');
 
+
+
+
       total += money;
     
       const html = `
+      <div class="show-date">${date}</div>
       <div class="show-money">${money}</div>
       <div class="show-todo">${todoName}</div>
-      <div class="show-date">${date}</div>
 
       <button class="remove-btn" onclick="
-        todoList.splice(${i}, 1);
-         localStorage.setItem('todoList', JSON.stringify(todoList));
+        expList.splice(${i}, 1);
+        adjustExp('${date}',${money});
+
+          localStorage.setItem('expList', JSON.stringify(expList));
+
         renderTodo ();
+        renderDateExp();
       ">Remove</button> 
     `;
-
       todoHTML += html;
-      totalItem.push(todoName);
+      if (todoName != ''){
+        totalItem.push(todoName);
+      }
 
     }
 
     document.querySelector('.js-show').innerHTML = todoHTML;
     document.querySelector('.total').innerText = 'Total: ' + total;
     document.querySelector('.totalItem').innerText = totalItem;
+
+}
+
+function renderDateExp(){
+
+  let dateHTML = '';
+
+  let first = true;
+
+
+  for (i=0; i < dateExp.length; i++){
+
+    let html;
+
+    if (first){
+      html = `
+    <div class="dateExp">Dateless</div>
+    <div class="expExp">${dateExp[i].exp}</div>
+    `;
+        first = false;
+      }
+    else{
+    html = `
+    <div class="dateExp">${dateExp[i].dateE}</div>
+    <div class="expExp">${dateExp[i].exp}</div>
+    `;}
+
+    dateHTML += html;
+
+  }
+  document.querySelector('.exp').innerHTML = dateHTML;
+
+}
+
+function adjustExp(date,money){
+
+  for (i=0; i < dateExp.length; i++){
+    if (dateExp[i].dateE === date){
+      dateExp[i].exp -= money;
+      localStorage.setItem('dateExp', JSON.stringify(dateExp));
+      return;
+    }
+  }
 
 }
 
