77b692934721a8d57e85ffa6ceeaf52296258578
AnoySarkar <123885890+AnoySarkar@users.noreply.github.com>
2025-02-16 11:39:06 +0530

    Add files via upload

---
+++ ExpenseList.js
@@ -0,0 +1,72 @@
+let todoList = [];
+let total = 0;
+
+  document.querySelector('.js-input')
+    .addEventListener("keydown",function (event){
+      if (event.key === "Enter"){
+        addTodo();
+      }
+    })
+
+
+
+function addTodo() {
+
+  let inputTodo = document.querySelector('.js-input');
+  let name = inputTodo.value;
+
+  if (name === '')
+    return;
+
+  let inputDate = document.querySelector('.js-input-date');
+  let date = inputDate.value;
+
+
+  todoList.push({
+    name,
+    date
+  });
+  
+  inputTodo.value = '';
+
+  renderTodo();
+}
+
+
+function renderTodo() {
+
+  let todoHTML = '';
+  total = 0;
+  
+    for (let i = 0; i < todoList.length; i++){
+      
+      let todoObject = todoList[i];
+      let {name , date} = todoObject;
+
+      let money = parseInt(name.replace(/\D/g, ""));
+      let todoName = name.replace(/\d/g, '');
+
+      total += money;
+      console.log(total);
+    
+      const html = `
+      <div class="show-money">${money}</div>
+      <div class="show-todo">${todoName}</div>
+      <div class="show-date">${date}</div>
+
+      <button class="remove-btn" onclick="
+        todoList.splice(${i}, 1);
+        renderTodo ();
+      ">Remove</button> 
+    `;
+
+      todoHTML += html;
+
+    }
+    document.querySelector('.js-show').innerHTML = todoHTML;
+
+    document.querySelector('.total').innerText = 'Total: ' + total;
+
+}
+
+  
