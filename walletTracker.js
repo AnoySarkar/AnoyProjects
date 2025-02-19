a32d31322521394e24482a844b3a7a38e62b4ff6
AnoySarkar <123885890+AnoySarkar@users.noreply.github.com>
2025-02-19 12:48:49 +0530

    Update walletTracker.js

--- walletTracker.js
+++ walletTracker.js
@@ -1,23 +1,17 @@
 let expList = JSON.parse(localStorage.getItem('expList')) || [];
-
 let dateExp = JSON.parse(localStorage.getItem('dateExp')) || [];
-
 let type;
 let typeD;
 let typeE;
-
 let sound = new Audio("restart2.wav");
-
 let entryType = document.querySelector('#type');
 
 function typeBack(){
   sound.play();
   let typeX = entryType.value;
-  
   if (typeX === 'inc'){
     entryType.style.backgroundColor = '#44d41d';
-  }
-  else{
+  } else {
     entryType.style.backgroundColor = 'rgb(251, 26, 26)';
   }
 }
@@ -35,8 +29,6 @@ document.querySelector('#inputName')
 document.addEventListener("DOMContentLoaded", function() {
   renderTodo();
   renderDateExp();
-
-  // Set today's date as default for inputDate
   const today = new Date().toISOString().split('T')[0];
   document.querySelector('#inputDate').value = today;
 });
@@ -44,36 +36,26 @@ document.addEventListener("DOMContentLoaded", function() {
 function addTodo() {
   let inputTodo = document.querySelector('#inputName');
   let name = inputTodo.value;
-
   if (name === '') return;
-
   let inputDate = document.querySelector('#inputDate');
   let date = inputDate.value;
   let money = parseInt(name.replace(/\D/g, ""));
-
   addDateExp(date, money);
   localStorage.setItem('dateExp', JSON.stringify(dateExp));
-
   expList.push({
     name,
     date,
     typeE
   });
-
   inputTodo.value = '';
-
   localStorage.setItem('expList', JSON.stringify(expList));
-
   renderTodo();
   renderDateExp();
 }
 
 function addDateExp(date, money) {
-  for (i = 0; i < dateExp.length; i++) {
-    let dateE = dateExp[i].dateE;
-    type = dateExp[i].type;
-
-    if (dateE === date) {
+  for (let i = 0; i < dateExp.length; i++) {
+    if (dateExp[i].dateE === date) {
       if (typeD === 'exp') {
         dateExp[i].exp += money;
         return;
@@ -88,14 +70,14 @@ function addDateExp(date, money) {
       dateE: date,
       exp: money,
       inc: 0,
-      type: typeD,
+      type: typeD
     });
   } else if (typeD === 'inc') {
     dateExp.push({
       dateE: date,
       exp: 0,
       inc: money,
-      type: typeD,
+      type: typeD
     });
   }
 }
@@ -105,15 +87,11 @@ function renderTodo() {
   let totalE = 0;
   let totalI = 0;
   let inOut;
-  
-  for (i = expList.length - 1; i >= 0; i--) {
-    type = expList[i].typeE;
-
-    let todoObject = expList[i];
-    let { name, date } = todoObject;
+  for (let i = expList.length - 1; i >= 0; i--) {
+    let type = expList[i].typeE;
+    let { name, date } = expList[i];
     let money = parseInt(name.replace(/\D/g, ""));
     let todoName = name.replace(/\d/g, '');
-
     if (type === 'exp') {
       totalE += money;
       inOut = '🟥';
@@ -121,7 +99,6 @@ function renderTodo() {
       totalI += money;
       inOut = '🟩';
     }
-
     const html = `
       <div id="date-lb">${date}</div>
       <div id="amount-lb">${money}</div>
@@ -130,17 +107,14 @@ function renderTodo() {
       <div id="remove-lb">
           <button class="remove-btn" onclick="
               sound.play();
-              adjustExp('${i}',${money});
+              adjustExp('${date}', '${type}', ${money});
               expList.splice(${i}, 1);
-              
               localStorage.setItem('expList', JSON.stringify(expList));
               renderTodo();
               renderDateExp();
-          ">Remove</button> 
+          ">Remove</button>
       </div>`;
-
     todoHTML += html;
-
     if (type === 'exp') {
       document.querySelector('#expense').innerText = totalE;
     } else if (type === 'inc') {
@@ -148,65 +122,51 @@ function renderTodo() {
     }
     document.querySelector('#onhand').innerText = totalI - totalE;
   }
-
   document.querySelector('#list-body').innerHTML = todoHTML;
 }
 
 function renderDateExp(){
-
   let dateHTML = '';
-
-  for (i=dateExp.length-1; i >= 0 ; i--){
-
-
+  for (let i = dateExp.length - 1; i >= 0; i--){
     let html;
-
-
-      if (i === 0){
-        html = `
-  
+    if (i === 0){
+      html = `
         <div id="date-db">Dateless</div>
         <div id="expense-db">${dateExp[i].exp}</div>
         <div id="income-db">${dateExp[i].inc}</div>
-      `;}
-      else{
+      `;
+    } else {
       html = `
         <div id="date-db">${dateExp[i].dateE}</div>
         <div id="expense-db">${dateExp[i].exp}</div>
         <div id="income-db">${dateExp[i].inc}</div>
-      `;}
-
-    
+      `;
+    }
     dateHTML += html;
-
   }
   document.querySelector('#date-body').innerHTML = dateHTML;
-
 }
 
-function adjustExp(i, money) {
+function adjustExp(date, type, money) {
   let totalE = parseInt(document.querySelector('#expense').innerText);
   let totalI = parseInt(document.querySelector('#total').innerText);
   let totalO = parseInt(document.querySelector('#onhand').innerText);
-
-  
-   let type = dateExp[i].type;
-
-    if (type === 'exp') {
-        dateExp[i].exp -= money;
-        localStorage.setItem('dateExp', JSON.stringify(dateExp));
+  for (let j = 0; j < dateExp.length; j++) {
+    if (dateExp[j].dateE === date) {
+      if (type === 'exp') {
+        dateExp[j].exp -= money;
         document.querySelector('#expense').innerText = totalE - money;
         document.querySelector('#onhand').innerText = totalO + money;
-    } 
-    else if (type === 'inc') {
-        dateExp[i].inc -= money;
-        localStorage.setItem('dateExp', JSON.stringify(dateExp));
+      } else if (type === 'inc') {
+        dateExp[j].inc -= money;
         document.querySelector('#total').innerText = totalI - money;
         document.querySelector('#onhand').innerText = totalO - money;
       }
-
- if (dateExp[i].exp === 0 && dateExp[i].inc === 0){
-   dateExp.splice(${i}, 1);
-   localStorage.setItem('dateExp', JSON.stringify(dateExp));
- }
-}
+      if (dateExp[j].exp === 0 && dateExp[j].inc === 0){
+        dateExp.splice(j, 1);
+      }
+      break;
+    }
+  }
+  localStorage.setItem('dateExp', JSON.stringify(dateExp));
+  }
