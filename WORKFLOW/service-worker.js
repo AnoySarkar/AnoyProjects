b87884f8c506e1f697c24c4725bc06bc66f2d098
AnoySarkar <123885890+AnoySarkar@users.noreply.github.com>
2026-04-19 23:53:43 +0530

    Add files via upload

---
+++ WORKFLOW/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'workflow-cache-v2';
+const LOCAL_ASSETS = [
+  './workflow.html',
+  './manifest.json',
+  './logo.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+const REMOTE_ASSETS = [
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      await cache.addAll(LOCAL_ASSETS);
+      await Promise.all(
+        REMOTE_ASSETS.map((url) =>
+          fetch(url)
+            .then((response) => {
+              if (response.ok) return cache.put(url, response.clone());
+              return null;
+            })
+            .catch(() => null)
+        )
+      );
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request)
+        .then((networkResponse) => {
+          if (networkResponse && networkResponse.ok) {
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+            });
+          }
+          return networkResponse;
+        })
+        .catch(() => caches.match('./workflow.html'));
+    })
+  );
+});
